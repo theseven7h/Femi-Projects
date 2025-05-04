7f25c986edb3243394e54858cbe92ee2c26dcbc8
theseven7h <jamestauri123@gmail.com>
2025-05-04 10:54:17 +0100

    Function done: displayPrimeNumbers

--- Sun.java
+++ Sun.java
@@ -2,4 +2,23 @@ public class Sun {
 	static int findMultiple(int number) {
 		return number * 5;
 	}
+
+	static void displayPrimeNumbers() {
+		int firstFifty = 0;
+		System.out.print("First 50 prime numbers are");
+		for (int number = 1; firstFifty < 50; number++) {
+			int count = number;
+			int prime = 0;
+			for (;count >= 1; count--) {
+				if (number % count == 0) prime++;
+			}
+			if (prime == 2) {
+				System.out.print(" " + number);
+				firstFifty++;
+			}
+		}
+	}
+
+
+
 }
--- SunTest.java
+++ SunTest.java
@@ -4,12 +4,13 @@ public class SunTest {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Enter number: ");
-		int number1 = input.nextInt();
+		//System.out.print("Enter number: ");
+		//int number1 = input.nextInt();
 		//int number2 = input.nextInt();
-
 		//Uncomment number2 input if needed
 
-		System.out.println(Sun.findMultiple(number1));
+		Sun.displayPrimeNumbers();
+
+		//System.out.println(Sun.findMultiple(number1));
 	}
 }
